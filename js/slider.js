29f86851d2e7cd2d6205c7f2c6cddd3e37fefe42
Mikan <orangecatsstudio@gmail.com>
2024-01-24 15:36:12 +0900

    when big window, it show 1 slick

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 2,//スライドを画面に1枚見せる
+    slidesToShow: 1,//スライドを画面に1枚見せる
     slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
     prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
     nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
